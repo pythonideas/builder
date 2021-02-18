19f226ba20a8589a81efa19583278f0bd070ed79
Rustbot Chessapp <rustbochessapp@gmail.com>
2021-02-18 11:58:06 +0000

    Spawn task

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const { watch } = require('fs')
+const child_process = require('child_process')
+
+function spawnSync(command, args){
+    console.log("spawn sync", command, args)
+    
+    const result = child_process.spawnSync(command, args)
+    
+    console.log(`stdout ${result.stdout}`)
+    console.log(`stderr ${result.stderr}`)
+    console.log(`status ${result.status}`)
+}
 
 watch("src", (event, filename) => {
-    console.log(event, event)
+    console.log("src", event, filename)
+    
+    spawnSync("npm", ["run", "compile"])
+})
+
+watch("dist", (event, filename) => {
+    console.log("dist", event, filename)
 })
